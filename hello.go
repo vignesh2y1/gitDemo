8f93c8ac51768a055802457b2676a415d2964f88
vignesh2y1 <vigneshfsd0@gmail.com>
2024-01-17 02:23:33 -0800

    next program

--- hello.go
+++ hello.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
+
 	fmt.Println("hello world")
+	fmt.Println(factorial(3))
+}
+
+func factorial(num int) int {
+	ans := 1
+	for i := 1; i <= num; i++ {
+		ans *= i
+	}
+	return ans
 }
